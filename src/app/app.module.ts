3068f9ff93bc9bc53dc3118e4d5f34df3b61196e
leandrofabianjr <leandrofabianjr@gmail.com>
2022-03-28 16:25:47 -0300

    Removido pacotes do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,63 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
-import { LogoutComponent } from './logout/logout.component';
-import { AngularFireAuthGuardModule } from '@angular/fire/compat/auth-guard';
-import { AuthorsComponent } from './authors/authors.component';
-import { GenresComponent } from './genres/genres.component';
-import { LocationsComponent } from './locations/locations.component';
-import { PublishersComponent } from './publishers/publishers.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ItemsComponent } from './items/items.component';
-import { MatPaginatorIntlPtbr } from './mat-paginator-intl-ptbr';
-import { ItemsNewComponent } from './items/items-new/items-new.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InfoErroCampoComponent } from './info-erro-campo/info-erro-campo.component';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { RealTimeSearchDirective } from './directives/real-time-search.directive';
-import { AuthorsNewComponent } from './authors/authors-new/authors-new.component';
-import { GenresNewComponent } from './genres/genres-new/genres-new.component';
-import { LocationsNewComponent } from './locations/locations-new/locations-new.component';
-import { PublishersNewComponent } from './publishers/publishers-new/publishers-new.component';
-import { DialogConfirmationComponent } from './dialog-confirmation/dialog-confirmation.component';
-import { TabColumnSearchComponent } from './tab-column-search/tab-column-search.component';
-import { DialogInfoComponent } from './dialog-info/dialog-info.component';
 import { A11yModule } from '@angular/cdk/a11y';
-// import { AdsenseModule } from 'ng2-adsense';
-import { BookOnlineSearchComponent } from './book-online-search/book-online-search.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LoggedComponent } from './logged/logged.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import {
   MatPaginatorIntl,
   MatPaginatorModule,
 } from '@angular/material/paginator';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSortModule } from '@angular/material/sort';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { AngularFireModule } from '@angular/fire/compat';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthorsNewComponent } from './authors/authors-new/authors-new.component';
+import { AuthorsComponent } from './authors/authors.component';
+// import { AdsenseModule } from 'ng2-adsense';
+import { BookOnlineSearchComponent } from './book-online-search/book-online-search.component';
+import { DialogConfirmationComponent } from './dialog-confirmation/dialog-confirmation.component';
+import { DialogInfoComponent } from './dialog-info/dialog-info.component';
+import { RealTimeSearchDirective } from './directives/real-time-search.directive';
+import { GenresNewComponent } from './genres/genres-new/genres-new.component';
+import { GenresComponent } from './genres/genres.component';
+import { InfoErroCampoComponent } from './info-erro-campo/info-erro-campo.component';
+import { ItemsNewComponent } from './items/items-new/items-new.component';
+import { ItemsComponent } from './items/items.component';
+import { LocationsNewComponent } from './locations/locations-new/locations-new.component';
+import { LocationsComponent } from './locations/locations.component';
+import { LoggedComponent } from './logged/logged.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MatPaginatorIntlPtbr } from './mat-paginator-intl-ptbr';
 import { PaginatorComponent } from './paginator/paginator.component';
+import { PublishersNewComponent } from './publishers/publishers-new/publishers-new.component';
+import { PublishersComponent } from './publishers/publishers.component';
+import { TabColumnSearchComponent } from './tab-column-search/tab-column-search.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -87,10 +80,6 @@ import { PaginatorComponent } from './paginator/paginator.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AngularFireAuthGuardModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
